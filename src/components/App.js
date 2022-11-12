493e5967dbbe76672bec74e1c0f542e6a34807e7
Salman khatik <110889430+salmank305@users.noreply.github.com>
2022-11-12 23:32:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,15 @@ import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
 const App = () => {
+  let setFunc;
+  let changeFunc = (func) => {
+    setFunc = func;
+  };
 
-  const changeFunc = () => {
-    
-  }
   const errorGenerator = () => {
-    
+    setFunc(true);
   }
-  
+
   return (
 
     <div id="main">
@@ -24,3 +25,4 @@ const App = () => {
 };
 
 export default App;
+
