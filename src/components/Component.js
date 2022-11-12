8f310dc950ba8301f5ab8c6cde0f0e56d6089560
Salman khatik <110889430+salmank305@users.noreply.github.com>
2022-11-12 23:31:28 +0530

    Update Component.js

--- src/components/Component.js
+++ src/components/Component.js
@@ -3,15 +3,16 @@ import React, { useState } from "react";
 const Component = (props) => {
     
     const [error, setError] = useState(false);
+    props.change(setError);
+    
+    if(error){
+      throw new Error('Error');
+    }
 
   return (
     <>
-    {    
-        error && <div>{somethingNotDefined}</div>
-    }
     </>
   );
 };
 
 export default Component;
-
